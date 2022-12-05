83762a6d9c9a1aa541ec5ca8e4ba2a9109068e6f
samlkrystof <ksaml@gapps.zcu.cz>
2022-12-05 10:18:34 +0100

    ConvNext block created

--- modules.py
+++ modules.py
@@ -77,3 +77,35 @@ class ResnetBlock(nn.Module):
 
         h = self.block2(h)
         return h + self.res_conv(x)
+
+class ConvNextBlock(nn.Module):
+    def __init__(self, dim, dim_out, *, time_emb_dim=None, mult=2, norm=True):
+        super(ConvNextBlock, self).__init__()
+        self.mlp(
+            nn.Sequential(nn.GELU(), nn.Linear(time_emb_dim, dim_out))
+            if time_emb_dim is not None
+            else None
+        )
+
+        self.first_conv = nn.Conv2d(dim, dim, 7, padding=3, groups=dim)
+        self.net = nn.Sequential(
+            nn.GroupNorm(1, dim) if norm else nn.Identity(),
+            nn.Conv2d(dim, dim_out * mult, 3, padding=1),
+            nn.GELU(),
+            nn.GroupNorm(1, dim_out * mult),
+            nn.Conv2d(dim_out * mult, dim_out, 3, padding=1),
+        )
+
+        self.res_conv = nn.Conv2d(dim, dim_out, 1) if dim != dim_out else nn.Identity()
+
+    def forward(self, x, time_emb=None):
+        h = self.ds_conv(x)
+
+        if self.mlp is not None and time_emb is not None:
+            condition = self.mlp(time_emb)
+            h = h + torch.unsqueeze(torch.unsqueeze(condition, 2), 3)
+
+        h = self.net(h)
+        return h + self.res_conv(x)
+
+
